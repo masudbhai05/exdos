34f534ff480e32ef71e58b74aa476d04c929888b
masudbhai05 <mdmasudali10102002@gmail.com>
2024-07-04 03:48:12 +0600

    Home page 2 responsive si done

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,9 +16,10 @@
 
     // about intro video Popup
     $('.popup-image').magnificPopup({
-        type: 'image'
+        type: 'image',
         // other options
     });
+   
     $('.popup-video').magnificPopup({
         type: 'iframe'
         // other options
@@ -42,23 +43,23 @@
     var tpSideMenu = $('.tp-offcanvas-menu nav');
     tpSideMenu.append(tpMenuWrap);
     if ($(tpSideMenu).find('.sub-menu, .tp-mega-menu').length != 0) {
-      $(tpSideMenu).find('.sub-menu, .tp-mega-menu').parent().append('<button class="tp-menu-close"><i class="fas fa-chevron-right"></i></button>');
+        $(tpSideMenu).find('.sub-menu, .tp-mega-menu').parent().append('<button class="tp-menu-close"><i class="fas fa-chevron-right"></i></button>');
     }
 
     var sideMenuList = $('.tp-offcanvas-menu nav > ul > li button.tp-menu-close, .tp-offcanvas-menu nav > ul li.has-dropdown > a');
     $(sideMenuList).on('click', function (e) {
-      console.log(e);
-      e.preventDefault();
-      if (!($(this).parent().hasClass('active'))) {
-        $(this).parent().addClass('active');
-        $(this).siblings('.sub-menu, .tp-mega-menu').slideDown();
-      } else {
-        $(this).siblings('.sub-menu, .tp-mega-menu').slideUp();
-        $(this).parent().removeClass('active');
-      }
+        console.log(e);
+        e.preventDefault();
+        if (!($(this).parent().hasClass('active'))) {
+            $(this).parent().addClass('active');
+            $(this).siblings('.sub-menu, .tp-mega-menu').slideDown();
+        } else {
+            $(this).siblings('.sub-menu, .tp-mega-menu').slideUp();
+            $(this).parent().removeClass('active');
+        }
     });
 
-     // Mobile menu code end here -------------------------------------------------------
+    // Mobile menu code end here -------------------------------------------------------
     // testimonial swiper 
     var swiper = new Swiper(".tp-testimonial-active", {
         slidesPerView: 1,
